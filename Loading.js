41c136e5d7befab6665e9a5359ddeb45052da430
VVVVVVVIN <77380046+VVVVVVVIN@users.noreply.github.com>
2021-04-28 02:35:25 +0900

    #2.0 - 210428

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Weather </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA",
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30,
+  },
+});
